763b8128f9442cc4fd60235e17521d5f440c549a
Glauco Pater <>
2018-11-04 17:09:05 +0100

    First deploy

---
+++ api-help.php
@@ -0,0 +1,63 @@
+<?
+error_reporting(E_ALL);
+define __DEFAULT_SEPARATOR__ = ",";
+
+/**
+ * fromIntToHexfrom 
+ *
+ * @param [string] $value
+ * @return [int] dechex($value) 
+ */
+function fromHexToInt($value) {
+    return dechex($value);
+}
+
+/**
+ * fromIntToHexfrom 
+ *
+ * @param [int] $value
+ * @return [string] hexdec($value) 
+ */
+function fromIntToHexfrom($value) {
+    return hexdec($value);
+}
+
+/**
+ * fromIntListToHexList 
+ *
+ * @param [string] $list
+ * @return [int] $list
+ */
+function fromHexListToIntList($list) {
+    return array_map("fromHexToInt", $list);
+}
+
+/**
+ * fromIntListToHexList 
+ *
+ * @param [int] $list
+ * @return [string] $list
+ */
+function fromIntListToHexList($list) {
+    return array_map("fromIntToHexfrom", $list);
+}
+
+
+if(isset($_REQUEST)) {
+    list($param1,$param2) = $_REQUEST;
+    $result = "";
+    switch($param1){
+        case "fromHexToInt": $result = fromHexToInt($param2); break;
+        case "fromIntToHexfrom": $result = fromHexToInt($param2); break;
+        case "fromHexListToIntList": $result = fromHexToInt($param2); break;
+        case "fromIntListToHexList": $result = fromHexToInt($param2); break;
+    }    
+    if (is_numeric($result) || is_string($result)){
+        echo $result;
+    } elseif (is_array($result) === "array") {
+        echo implode(",", $result);
+    }
+}
+
+
+?>
