52b402a7b7e260610c84372912f7e88e3b3e18d0
Glauco Pater <>
2018-11-04 19:00:41 +0100

    Fix issues

--- api-help.php
+++ api-help.php
@@ -1,26 +1,26 @@
 <?
 error_reporting(E_ALL);
 define("__DEFAULT_SEPARATOR__", ",");
-
+define("__API_KEY__", "api_key");
 
 /**
- * fromIntToHexfrom 
+ * fromHexToInt 
  *
  * @param [string] $value
  * @return [int] dechex($value) 
  */
 function fromHexToInt($value) {
-    return dechex($value);
+    return hexdec($value);
 }
 
 /**
- * fromIntToHexfrom 
+ * fromIntToHex 
  *
  * @param [int] $value
  * @return [string] hexdec($value) 
  */
-function fromIntToHexfrom($value) {
-    return hexdec($value);
+function fromIntToHex($value) {
+    return "#".dechex($value);
 }
 
 /**
@@ -40,25 +40,27 @@ function fromHexListToIntList($list) {
  * @return [string] $list
  */
 function fromIntListToHexList($list) {
-    return array_map("fromIntToHexfrom", $list);
+    return array_map("fromIntToHex", $list);
 }
 
-
-if(isset($_REQUEST)) {
-    list($param1,$param2) = $_REQUEST;
-    $result = "";
-    switch($param1){
-        case "fromHexToInt": $result = fromHexToInt($param2); break;
-        case "fromIntToHexfrom": $result = fromHexToInt($param2); break;
-        case "fromHexListToIntList": $result = fromHexToInt($param2); break;
-        case "fromIntListToHexList": $result = fromHexToInt($param2); break;
-    }    
-    if (is_numeric($result) || is_string($result)){
-        echo $result;
-    } elseif (is_array($result) === "array") {
-        echo implode(__DEFAULT_SEPARATOR__, $result);
+$request = $_REQUEST;
+if (isset($request[__API_KEY__])) {
+    if(count($request) > 0)
+    {
+        array_shift($request);
+        $param1 = array_keys($request)[0];
+        $value1 = $request[$param1];
+        switch($param1){
+            case "fromHexToInt": $result = fromHexToInt($value1); break;
+            case "fromIntToHex": $result = fromIntToHex($value1); break;
+            case "fromHexListToIntList": $result = fromHexListToIntList(explode(__DEFAULT_SEPARATOR__,$value1)); break;
+            case "fromIntListToHexList": $result = fromIntListToHexList(explode(__DEFAULT_SEPARATOR__,$value1)); break;
+            default: $result = null;
+        }    
+        if (is_numeric($result) || is_string($result)){
+            echo $result;
+        } elseif (is_array($result)) {
+            echo implode(__DEFAULT_SEPARATOR__, $result);
+        }
     }
 }
-
-
-?>
